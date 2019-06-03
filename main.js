7aae5a0d9d5360e3b79c19fb88e536a3acb94fb7
Chris McGivney <cmmcgivney84@gmail.com>
2019-06-03 13:28:43 -0600

    still confused on this project

---
+++ main.js
@@ -0,0 +1,27 @@
+let question = document.getElementById('content q');
+let answer = document.getElementById('content a');
+var questions = ["Test Questions1", "Test Question2"];
+var answers = ["Test Answers1", "Test Answer2"];
+
+//
+
+
+
+function getAnswer() {
+for (var i = 0; i < answers.length; i++) {
+  answers[0] = answer
+  console.log(answers[i]);
+}
+}
+function getQuestion() {
+for (var i = 0; i < questions.length; i++) {
+  console.log(questions[i]);
+}
+}
+
+
+console.log(getAnswer());
+// console.log(getQuestion());
+
+
+
