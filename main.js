9f36626f7f67eb98d2d3380574bf0fd3e96c0b06
Chris McGivney <cmmcgivney84@gmail.com>
2019-06-03 14:29:34 -0600

    added semantic

--- main.js
+++ main.js
@@ -9,19 +9,20 @@ var answers = ["Test Answers1", "Test Answer2"];
 
 function getAnswer() {
 for (var i = 0; i < answers.length; i++) {
-  answers[0] = answer
+  answer.innerHTML = answers[0]
   console.log(answers[i]);
 }
 }
 function getQuestion() {
 for (var i = 0; i < questions.length; i++) {
+  question.innerHTML = questions[0]
   console.log(questions[i]);
 }
 }
 
 
 console.log(getAnswer());
-// console.log(getQuestion());
+console.log(getQuestion());
 
 
 
